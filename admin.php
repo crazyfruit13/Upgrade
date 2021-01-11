d217663e7774f5c1893efe0f7f0454640ccc58a8
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-11 17:47:09 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -33,14 +33,13 @@
   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
   <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
 </svg></label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Username To Add">
-      <div class="col-auto my-1">
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="form-row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Username">
     </div>
     </div>
-    </div>
-    </form>
+  <button type="submit" class="btn btn-success">Confirm</button>
+</form>
      <div>
       <h1> Remove User</h1> 
       <form>
@@ -49,14 +48,13 @@
   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
   <path fill-rule="evenodd" d="M12.146 5.146a.5.5 0 0 1 .708 0L14 6.293l1.146-1.147a.5.5 0 0 1 .708.708L14.707 7l1.147 1.146a.5.5 0 0 1-.708.708L14 7.707l-1.146 1.147a.5.5 0 0 1-.708-.708L13.293 7l-1.147-1.146a.5.5 0 0 1 0-.708z"/>
 </svg></label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control form-control-lg" id="colFormLabelLg" placeholder="Username To Remove">
-      <div class="col-auto my-1">
-      <button type="submit" class="btn btn-primary">Submit</button>
+   <div class="form-row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="Username">
     </div>
     </div>
-    </div>
-    </form>
+  <button type="submit" class="btn btn-danger">Confirm</button>
+</form>
     <div>
       <h1> Adjust Kowskicoins</h1>
       <form>
@@ -64,25 +62,13 @@
     <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-wallet2" viewBox="0 0 16 16">
   <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
 </svg></label>
-    <form class="form-inline">
-  <label class="sr-only" for="inlineFormInputName2">Username</label>
-  <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe">
-
-  <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-  <div class="input-group mb-2 mr-sm-2">
-    <div class="input-group-prepend">
-      <div class="input-group-text">$</div>
-    </div>
-    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Kowskicoin Total">
-  </div>
-      <div class="col-auto my-1">
-      <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="form-row">
+    <div class="col">
+      <input type="text" class="form-control" placeholder="New Total">
     </div>
     </div>
-    </div>
-    </div>
-    </form>
-    </div>
+  <button type="submit" class="btn btn-info">Confirm</button>
+</form>
     </body>
 <!-- Footer -->
 <footer class="bg-dark text-center text-lg-start">
