b7834f3d2ecd82c26827b2f5edde8e8d354fdf3a
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-11 15:21:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <?php
     require("../data/mysql.php");
 ?>
-<html>
-    <body>
-    	<h1> Kowskicoin Database </h1>
-          <?php
-                $query = "select username,coinval FROM Users, Kowskicoin WHERE Users_idUsers = idUsers ORDER BY Users_idUsers;";
-                $result = mysqli_query($db, $query);
-                while($row = mysqli_fetch_assoc($result)) {
-                      // Display your datas on the page
-                }
-          ?>
-    </body>
-</html>
+echo"<html>"
+    echo"<body>"
+    	echo"<h1> Kowskicoin Database </h1>"
+        echo "<table border='1'>
 
+        <tr>
+
+        <th>Username</th>
+
+        <th>Coin Total</th>
+
+        </tr>";
+          <?php
+                $query = "select Username,coinval FROM Users, Kowskicoin WHERE Users_idUsers = idUsers ORDER BY coinval DESC;";
+                $result = mysqli_query($dbase, $query);
+                while($row = mysqli_fetch_assoc($result)) {
+                      echo "<tr>";
+
+                      echo "<td>" . $row['Username'] . "</td>";
+
+                      echo "<td>" . $row['coinval'] . "</td>";      
+
+                      echo "</tr>";
+                }
+        echo "</table>";
+    echo "</body>"
+echo "</html>"
+>?
