2eb0a418704e7fb57817f710ed0718c1f05d546f
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-11 17:08:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
 <!-- Footer -->
 <footer class="bg-dark text-center text-lg-start">
 <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+  <div class="text-center text-light p-3" style="background-color: rgba(0, 0, 0, 0.2)">
     © 2021 Copyright:
     <a class="text-light" href="https://mdbootstrap.com/">Arktech Consulting Group, LLC</a>
     </div>
