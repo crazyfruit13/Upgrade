ae1846df78882d5cd0e487f46c40169a2115dfaa
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 02:10:06 -0500

    Update post.php

--- post.php
+++ post.php
@@ -8,13 +8,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $totaluser =  $_POST['totaluser'];
     $total =  $_POST['total'];
     if (isset($total)) {
-     $query = $mysqli->query("UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
+     mysqli_real_query($db, "UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
      alert("Total successfully adjusted!");
     }elseif (isset($userdel)) {
-     $query = $mysqli->query("DELETE from kowskicoin WHERE Username= '$userdel'");
+     mysqli_real_query($db, "DELETE from kowskicoin WHERE Username= '$userdel'");
      alert("User successfully deleted!");
     }elseif (isset($user)) {
-     $query = $mysqli->query("insert Users values (0, '$user')");
+    mysqli_real_query($db, "insert Users values (0, '$user')");
      alert("User successfully added!");
     }else {
         alert('Please select an option!');
