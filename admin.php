a895d324bdfd943cc4b845b2b4da65d21e6bc700
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 02:11:15 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,28 @@
-   <html>
+<?php
+    $db = mysqli_connect('localhost','root','E68}eb%.@>8+LNMA','kowskicoin')
+ or die('Error connecting to MySQL server.');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user =  $_POST['adduser'];
+    $userdel =  $_POST['removeuser'];
+    $totaluser =  $_POST['totaluser'];
+    $total =  $_POST['total'];
+    if (isset($total)) {
+     mysqli_real_query($db, "UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
+     alert("Total successfully adjusted!");
+    }elseif (isset($userdel)) {
+     mysqli_real_query($db, "DELETE from kowskicoin WHERE Username= '$userdel'");
+     alert("User successfully deleted!");
+    }elseif (isset($user)) {
+    mysqli_real_query($db, "insert Users values (0, '$user')");
+     alert("User successfully added!");
+    }else {
+        alert('Please select an option!');
+    } 
+ }
+ 
+ ?>   
+<html>
     <head>
          <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -26,7 +50,7 @@
     <body style="background-color: #343A40">
     <div>
       <h1> Add User</h1>
-      <form name="adduser" action="post.php" method="post">
+      <form name="adduser" action="admin.php" method="post">
       <div class="form-group row">
     <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
@@ -40,7 +64,7 @@
 </form>
      <div>
       <h1> Remove User</h1> 
-      <form name="deluser" action="post.php" method="post">
+      <form name="deluser" action="admin.php" method="post">
       <div class="form-group row">
     <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-x" viewBox="0 0 16 16">
   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
