7c584b2216305e6cafa7795320d1b4a66571a5ea
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-11 20:09:27 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,21 +1,20 @@
 <?php
     $db = mysqli_connect('localhost','root','E68}eb%.@>8+LNMA','kowskicoin')
  or die('Error connecting to MySQL server.');
-?>
-<?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user =  $_POST['adduser'];
     $userdel =  $_POST['removeuser'];
     $totaluser =  $_POST['totaluser'];
     $total =  $_POST['total'];
     if (isset($total)) {
-        $query = "UPDATE Kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser' ";
+        $query = $mysqli->query("UPDATE Kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser')";
         alert("Total successfully adjusted!"); 
     }elseif (isset($userdel) {
-        $query = "DELETE from Kowskicoin WHERE Username= '$userdel' ";
+        $query = $mysqli->query("DELETE from Kowskicoin WHERE Username= '$userdel' ");
         alert("User successfully deleted!"); 
     }elseif (isset($user) {
-        $query = "insert into Users values (0, '$user')";
+        $query = $mysqli->query("insert Users values (0, '$user')");
         alert("User successfully added!"); 
     }else {
         alert('Please select an option!');
