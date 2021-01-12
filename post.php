147348580dc417cb9cf4252d556df82cfcdb80e6
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 04:13:51 -0500

    Update post.php

--- post.php
+++ post.php
@@ -7,7 +7,7 @@
     $userdel =  $_POST['removeuser'];
     $totaluser =  $_POST['totaluser'];
     $total =  $_POST['total'];
-    if (isset($user)) {
+    if (isset($total)) {
       $checksum = 1;
     }
     if ($checksum = 1) {
