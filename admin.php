25da27d49df88b3907c9e64318b38e2903997f13
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 01:17:40 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -19,8 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         alert("User successfully added!"); 
     }else {
         alert('Please select an option!');
-    }
- } 
+    } 
  }
  ?>            
 <html>
