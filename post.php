b6ee30d070553d20b2e97c168e1b6ca16a048864
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 05:56:42 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,37 +1,44 @@
-  <?php
-  function val($user, $userdel, $totaluser, $total) {
-     if (!empty($total)) {
-      if (mysqli->query("UPDATE kowskicoin, Users SET coinval=" $total "WHERE Username =" $totaluser) === TRUE) {
-        $message = "Total successfully adjusted!";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-      }
-      else {
-        printf("Error: %s\n", $mysqli->error);
-      }
-     }elseif (!empty($userdel)) {
-       if (mysqli->query("DELETE from kowskicoin WHERE Username=" $userdel) === TRUE) {
-        $message = "User successfully deleted!";
-        echo "<script type='text/javascript'>alert('$message');</script>";
-      }
-      else {
-        printf("Error: %s\n", $mysqli->error);
-      }
-    }elseif (!empty($user)) {
-       if (mysqli->query("insert Users values (0," $user)) === TRUE) {
+    <?php
+      $mysqli = mysqli_connect('localhost','root','E68}eb%.@>8+LNMA','kowskicoin')
+ or die('Error connecting to MySQL server.');
+  function adduser($user) {
+        if ($mysqli->query("insert Users values (0," .$user.")") === TRUE) {
         $message = "User successfully adjusted!";
         echo "<script type='text/javascript'>alert('$message');</script>";
-      }
-      else {
+      }else {
         printf("Error: %s\n", $mysqli->error);
       }  
+ }
+ 
+  function deluser($userdel) {
+       if ($mysqli->query("DELETE from kowskicoin WHERE Username=" .$userdel) === TRUE) {
+        $message = "User successfully deleted!";
+        echo "<script type='text/javascript'>alert('$message');</script>";
+      }else {
+        printf("Error: %s\n", $mysqli->error);
+  }
+}
+  function adjust($total, $totaluser) {
+     if ($mysqli->query("UPDATE kowskicoin, Users SET coinval=" .$total. "WHERE Username =" .$totaluser) === TRUE) {
+     $message = "Total successfully adjusted!";
+     echo "<script type='text/javascript'>alert('$message');</script>";
+      }else {
+        printf("Error: %s\n", $mysqli->error);
+  }
+}
+  function val($user, $userdel, $totaluser, $total) {
+     if (!empty($total)) {
+       adjust($total, $totaluser)
+    }elseif (!empty($userdel)) {
+       deluser($userdel)
+    }elseif (!empty($user)) {
+       adduser($user)
     }else {
         echo "<script type='text/javascript'>alert(Please select an option!);</script>";
     }
      return $result == true;
-     mysqli->close($db);
+     $mysqli->close($mysqli);
   }
-    $db = mysqli_connect('localhost','root','E68}eb%.@>8+LNMA','kowskicoin')
- or die('Error connecting to MySQL server.');
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user =  $_POST['adduser'];
     $userdel =  $_POST['removeuser'];
