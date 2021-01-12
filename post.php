ee0b10669f3393a937e562683399b6cceb9bad7f
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 02:03:05 -0500

    Update post.php

--- post.php
+++ post.php
@@ -2,30 +2,20 @@
     $db = mysqli_connect('localhost','root','E68}eb%.@>8+LNMA','kowskicoin')
  or die('Error connecting to MySQL server.');
 
- function adjusttotal() {
-     $query = $mysqli->query("UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
-     alert("Total successfully adjusted!");
- }
- function deluser() {
-     $query = $mysqli->query("DELETE from kowskicoin WHERE Username= '$userdel'");
-     alert("User successfully deleted!");
- }
- function adduser() {
-     $query = $mysqli->query("insert Users values (0, '$user')");
-     alert("User successfully added!"); 
- }
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user =  $_POST['adduser'];
     $userdel =  $_POST['removeuser'];
     $totaluser =  $_POST['totaluser'];
     $total =  $_POST['total'];
     if (isset($total)) {
-        adustotal(); 
+     $query = $mysqli->query("UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
+     alert("Total successfully adjusted!");
     }elseif (isset($userdel)) {
-        deluser();
+     $query = $mysqli->query("DELETE from kowskicoin WHERE Username= '$userdel'");
+     alert("User successfully deleted!");
     }elseif (isset($user)) {
-        adduser();
+     $query = $mysqli->query("insert Users values (0, '$user')");
+     alert("User successfully added!");
     }else {
         alert('Please select an option!');
     } 
