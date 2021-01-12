3de0feeabaf025d4d4ae8ee3c3814feb19466d98
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 03:22:03 -0500

    Update post.php

--- post.php
+++ post.php
@@ -14,8 +14,9 @@
      $result = real_query("DELETE from kowskicoin WHERE Username= '$userdel'");
      echo "<script type='text/javascript'>alert(User successfully deleted!);</script>";
     }elseif (!empty($user)) {
-     $result = real_query("insert Users values (0, '$user')");
      echo "<script type='text/javascript'>alert(User successfully added!);</script>";
+     $result = real_query("insert Users values (0, '$user')");
+     
     }else {
         echo "<script type='text/javascript'>alert(Please select an option!);</script>";
     } 
