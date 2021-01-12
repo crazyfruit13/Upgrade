455f8f771eeed4c046660b4b328b76acda5d13e0
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 04:30:55 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,11 +1,12 @@
   <?php
   function val() {
+     if (!empty($_POST['total'])) {
      $result = real_query("UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
      echo "<script type='text/javascript'>alert(Total successfully adjusted!);</script>";
-     if (!empty($_POST['removeuser'])) {
+     }elseif (!empty($_POST['removeuser'])) {
      $result = real_query("DELETE from kowskicoin WHERE Username= '$userdel'");
      echo "<script type='text/javascript'>alert(User successfully deleted!);</script>";
-    }elseif (!empty($_POST['total'])) {
+    }elseif (!empty($_POST['adduser'])) {
      $result = real_query("insert Users values (0, '$user')");
      echo "<script type='text/javascript'>alert(User successfully added!);</script>";
      
