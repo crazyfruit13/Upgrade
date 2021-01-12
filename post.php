dad1e6fe274803e3f416b97687c2bb4cec3ebe4e
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 02:46:54 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,4 +1,4 @@
-<?php
+  <?php
 require 'admin.php';
 
     $db = mysqli_connect('localhost','root','E68}eb%.@>8+LNMA','kowskicoin')
@@ -9,18 +9,18 @@ if  (isset($_POST['submit'] ) ) {
     $userdel =  $_POST['removeuser'];
     $totaluser =  $_POST['totaluser'];
     $total =  $_POST['total'];
-    echo 'alert('$user, $userdel, $totaluser, $total')'
+    echo "<script type='text/javascript'>alert('$user, $userdel, $totaluser, $total');</script>";
     if (isset($total)) {
      $result = real_query("UPDATE kowskicoin, Users SET coinval= $total WHERE Username = '$totaluser'");
-     echo 'alert("Total successfully adjusted!")';
+     echo "<script type='text/javascript'>alert(Total successfully adjusted!);</script>";
     }elseif (isset($userdel)) {
      $result = real_query("DELETE from kowskicoin WHERE Username= '$userdel'");
-     echo 'alert("User successfully deleted!")';
+     echo "<script type='text/javascript'>alert(User successfully deleted!);</script>";
     }elseif (isset($user)) {
      $result = real_query("insert Users values (0, '$user')");
-     echo 'alert("User successfully added!")';
+     echo "<script type='text/javascript'>alert(User successfully added!);</script>";
     }else {
-        echo 'alert('Please select an option!')';
+        echo "<script type='text/javascript'>alert(Please select an option!);</script>";
     } 
  }
  return $result == true;
