b894423f39f898d7ddd67fa817ae2c445be61f87
Jonathan Matson <30379290+crazyfruit13@users.noreply.github.com>
2021-01-12 01:50:20 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <body style="background-color: #343A40">
     <div>
       <h1> Add User</h1>
-      <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+      <form method="post" action="post">
       <div class="form-group row">
     <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
   <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
@@ -109,6 +109,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
      alert("User successfully deleted!");
  }
  function adduser() {
+     $query = $mysqli->query("insert Users values (0, '$user')");
      alert("User successfully added!"); 
  }
  ?>
